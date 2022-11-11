30a9aaafca63ed3a87e35c4af05b06b2850ae094
Jan Wozniak <wozniak.jan@gmail.com>
2022-11-11 11:24:23 +0100

    Dependabot helper script

---
+++ hack/dependabot_helper.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIR=$(dirname "${BASH_SOURCE[0]}")/..
+
+# list open PRs from dependabot touching KEB go modules
+prs=$(gh pr list --json number,author,title)
+
+# iterate over each PR, run go mod tidy under the KCP CLI dir, commit, push
+git checkout main
+git pull origin main
+declare -A prMap
+
+while read pr; do
+    mod=$(echo "$pr" | jq '.title' | sed -e 's/.*Bump \([^ ]*\) .*/\1/')
+    prMap[$mod]="$pr"
+done < <(echo "${prs}" | jq -c '.[]')
+
+function merge() {
+    mod=$1
+    if [[ "${prMap[$mod]+x}" ]]; then
+        echo merging $mod
+        pr=$(echo ${prMap[$mod]} | jq '.number')
+        while ! gh pr view 50 --json 'mergeable' | grep -e 'MERGEABLE' -e 'UNKNOWN'; do
+            sleep 5
+        done
+        gh pr review "${pr}" --approve
+        gh pr merge "$pr" --auto --merge
+        unset prMap[$mod]
+    fi
+    
+}
+
+merge "k8s.io/apimachinery"
+merge "k8s.io/api"
+merge "k8s.io/client-go"
+
+for k in "${!prMap[@]}"; do
+    merge "$k"
+done
